4ca3b210ea744be2f63ca832b271ee5637fc743e
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-12-08 21:20:04 -0500

    final push of class with front-end Monolith

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 // if port environment variable doesnt exist, use 9000
 const port = process.env.PORT || 9000;
 
 app.use(cors());
 app.use(express.json())
-app.use("/api/", (req, res) => {
+app.use(express.static(path.join(__dirname,"client/build")))
+
+app.use("/api/*", (req, res) => {
   res.json({
     data: "API Data Served!"
   })
@@ -17,6 +20,10 @@ app.use("/api/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is on port: ${port}`)
+})
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"))
 })
 
 
